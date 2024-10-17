5c52d1f52185e34db0c636ce5cae6d271074d4ab
Jeonyb00 <jyb000507@naver.com>
2024-10-17 17:43:33 +0900

    project

---
+++ src/Pages/UserlmageList.jsx
@@ -0,0 +1,199 @@
+import React from "react";
+import  LinearArrowsActionLogin21  from "../Icons/LinearArrowActionLogin21/LinearArrowActionLogin21";
+import  LinearMessagesConversationChatRoundMoney1  from "../Icons/LinearMessagesConversationChatRoundMoney1/LinearMessagesConversationChatRoundMoney1";
+import  LinearNotificationsBell  from "../Icons/LinearNotificationsBell/LinearNotificationsBell";
+import  LinearUsersUserRounded1  from "../Icons/LinearUserUserRounded1/LinearUsersUserRounded1";
+import "./Style.css";
+
+function UserImageList () {
+  return (
+    <div className="user-image-list">
+      <div className="overlap">
+        <div className="user-back-ground" />
+        <div className="category">
+          <div className="overlap-group">
+            <img className="line" alt="Line" src="/img/line-24.svg" />
+            <div className="frame">
+              <div className="text-wrapper">열람 관리</div>
+            </div>
+            <div className="div-wrapper">
+              <div className="text-wrapper">사진</div>
+            </div>
+            <div className="div">
+              <div className="text-wrapper">게시물</div>
+            </div>
+            <div className="frame-2">
+              <div className="text-wrapper">정보</div>
+            </div>
+            <div className="frame-3">
+              <div className="text-wrapper">친구</div>
+            </div>
+          </div>
+        </div>
+        <div className="user-header">
+          <div className="user-info">
+            <div className="user-name">
+              <div className="frame-4">
+                <div className="text-wrapper-2">구독중</div>
+              </div>
+              <div className="text-wrapper-3">이원준님</div>
+            </div>
+            <div className="frame-5">
+              <div className="frame-4">
+                <div className="text-wrapper-2">보유코인</div>
+              </div>
+              <div className="text-wrapper-3">2000AC</div>
+            </div>
+          </div>
+          <div className="overlap-2">
+            <div className="ellipse" />
+            <img className="male-user" alt="Male user" src="/img/male-user.png" />
+            <div className="ellipse-2" />
+            <img className="camera" alt="Camera" src="/img/camera.png" />
+          </div>
+        </div>
+      </div>
+      <div className="main">
+        <div className="user-profile">
+          <div className="calender">
+            <div className="OCTOBER"> OCTOBER 2024</div>
+            <div className="day">
+              <div className="text-wrapper-4">WE</div>
+              <div className="text-wrapper-5">TU</div>
+              <div className="text-wrapper-6">Mo</div>
+              <div className="text-wrapper-7">TH</div>
+              <div className="text-wrapper-8">FR</div>
+              <div className="text-wrapper-9">SA</div>
+              <div className="text-wrapper-10">SU</div>
+              <img className="img" alt="Line" src="/img/line-31.svg" />
+            </div>
+            <div className="date">
+              <div className="text-wrapper-11">1</div>
+              <div className="text-wrapper-12">2</div>
+              <div className="text-wrapper-13">3</div>
+              <div className="text-wrapper-14">4</div>
+              <div className="text-wrapper-15">5</div>
+              <div className="text-wrapper-16">6</div>
+              <div className="text-wrapper-17">13</div>
+              <div className="text-wrapper-18">12</div>
+              <div className="text-wrapper-19">11</div>
+              <div className="text-wrapper-20">10</div>
+              <div className="text-wrapper-21">9</div>
+              <div className="text-wrapper-22">8</div>
+              <div className="text-wrapper-23">7</div>
+              <div className="text-wrapper-24">14</div>
+              <div className="text-wrapper-25">15</div>
+              <div className="text-wrapper-26">16</div>
+              <div className="text-wrapper-27">21</div>
+              <div className="text-wrapper-28">22</div>
+              <div className="text-wrapper-29">23</div>
+              <div className="text-wrapper-30">17</div>
+              <div className="text-wrapper-31">18</div>
+              <div className="text-wrapper-32">19</div>
+              <div className="text-wrapper-33">20</div>
+              <div className="text-wrapper-34">27</div>
+              <div className="text-wrapper-35">26</div>
+              <div className="text-wrapper-36">25</div>
+              <div className="text-wrapper-37">24</div>
+              <div className="text-wrapper-38">28</div>
+              <div className="text-wrapper-39">29</div>
+              <div className="text-wrapper-40">30</div>
+              <div className="text-wrapper-41">31</div>
+            </div>
+          </div>
+          <img className="line-2" alt="Line" src="/img/line-25.svg" />
+          <img className="line-3" alt="Line" src="/img/line-26.svg" />
+          <div className="title">
+            <div className="text-wrapper-42">사진</div>
+          </div>
+          <div className="overlap-3">
+            <div className="image-frame">
+              <div className="overlap-group-2">
+                <div className="text-wrapper-43">UserImg1</div>
+              </div>
+            </div>
+            <div className="overlap-wrapper">
+              <div className="overlap-group-2">
+                <div className="text-wrapper-43">UserImg1</div>
+              </div>
+            </div>
+            <div className="overlap-group-wrapper">
+              <div className="overlap-group-2">
+                <div className="text-wrapper-43">UserImg1</div>
+              </div>
+            </div>
+            <div className="image-frame-2">
+              <div className="overlap-group-2">
+                <div className="text-wrapper-43">UserImg1</div>
+              </div>
+            </div>
+            <div className="image-frame-3">
+              <div className="overlap-group-2">
+                <div className="text-wrapper-43">UserImg1</div>
+              </div>
+            </div>
+            <div className="image-frame-4">
+              <div className="overlap-group-2">
+                <div className="text-wrapper-43">UserImg1</div>
+              </div>
+            </div>
+            <div className="image-frame-5">
+              <div className="overlap-group-2">
+                <div className="text-wrapper-43">UserImg1</div>
+              </div>
+            </div>
+            <div className="image-frame-6">
+              <div className="overlap-group-2">
+                <div className="text-wrapper-43">UserImg1</div>
+              </div>
+            </div>
+            <div className="image-frame-7">
+              <div className="overlap-group-2">
+                <div className="text-wrapper-43">UserImg1</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="navbar">
+        <div className="container">
+          <div className="text-wrapper-44">맛집 찾기</div>
+        </div>
+        <div className="container-2">
+          <div className="text-wrapper-45">일기 보기</div>
+        </div>
+        <div className="form">
+          <div className="overlap-group-3">
+            <div className="input">
+              <div className="container-3">
+                <div className="text-wrapper-46">검색어를 입력해 주세요.</div>
+              </div>
+            </div>
+            <img className="SVG" alt="Svg" src="/img/svg.svg" />
+          </div>
+        </div>
+        <div className="text-wrapper-47">로그인</div>
+        <div className="text-wrapper-48">코인결제</div>
+        <div className="text-wrapper-49">구독신청</div>
+        <div className="text-wrapper-50">마이</div>
+        <div className="clip-path-group-wrapper">
+          <div className="clip-path-group">
+            <div className="group">
+              <div className="overlap-group-4">
+                <div className="element" />
+                <div className="text-wrapper-51">AMADDA!</div>
+                <div className="FOOD-DIARY">FOOD &amp; DIARY</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <LinearArrowsActionLogin21 className="linear-arrows-action" />
+        <LinearUsersUserRounded1 className="linear-users-user" />
+        <LinearNotificationsBell className="linear-notifications" />
+        <LinearMessagesConversationChatRoundMoney1 className="linear-messages" />
+      </div>
+    </div>
+  );
+};
+
+export default UserImageList;
